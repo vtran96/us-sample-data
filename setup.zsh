1c96433f683fe41005d3cb08de089dda250381c8
van888 <van888@gmail.com>
2023-10-27 23:06:06 -0400

    initial commit

---
+++ setup.zsh
@@ -0,0 +1,20 @@
+echo 'Running Project: us-sample-data'
+
+python3 -m pip install --upgrade pip
+
+source /Users/vantran/Projects/python/us-sample-data/.venv/bin/activate
+
+python3 -m pip install --upgrade pip
+
+python3 -m pip install wheel
+
+pip3 install -r ./us-sample-data/requirements.txt
+
+/Applications/Visual\ Studio\ Code\ -\ Insiders.app/Contents/Resources/app/bin/code /Users/vantran/Projects/python/us-sample-data
+
+/Applications/Visual\ Studio\ Code\ -\ Insiders.app/Contents/Resources/app/bin/code /Users/vantran/Projects/python/us-sample-data/main.py
+
+echo /Applications/Visual\ Studio\ Code\ -\ Insiders.app/Contents/Resources/app/bin/code /Users/vantran/Projects/python/us-sample-data
+
+echo /Applications/Visual\ Studio\ Code\ -\ Insiders.app/Contents/Resources/app/bin/code /Users/vantran/Projects/python/us-sample-data/main.py
+
